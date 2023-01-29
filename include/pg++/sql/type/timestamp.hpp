b3c788a79c92c8a615262397ab64e30c550ad7f7
Gene Koval <genyaremchuk@protonmail.com>
2023-01-28 20:51:28 -0500

    Have timestamp support all durations

--- include/pg++/sql/type/timestamp.hpp
+++ include/pg++/sql/type/timestamp.hpp
@@ -1,28 +1,68 @@
 #pragma once
 
-#include "../sql.hpp"
+#include "int.hpp"
 
 #include <chrono>
 
-namespace pg {
-    using timestamp = std::chrono::system_clock::time_point;
+namespace pg::detail {
+    constexpr auto epoch_offset = [] {
+        using std::chrono::January;
+        using std::chrono::duration_cast;
+        using std::chrono::microseconds;
+        using std::chrono::sys_days;
 
-    template <>
-    struct type<timestamp> {
+        constexpr auto pg_epoch = January / 1 / 2000;
+
+        constexpr auto time_since_epoch =
+            sys_days(pg_epoch).time_since_epoch();
+
+        return duration_cast<microseconds>(time_since_epoch);
+    }();
+}
+
+namespace pg {
+    template <typename Duration>
+    struct type<std::chrono::time_point<std::chrono::system_clock, Duration>> {
+        using time_point =
+            std::chrono::time_point<std::chrono::system_clock, Duration>;
+
         static constexpr std::int32_t oid = 1184;
         static constexpr std::string_view name = "timestamptz";
 
         static auto from_sql(
             std::int32_t size,
             reader& reader
-        ) -> ext::task<timestamp>;
+        ) -> ext::task<time_point> {
+            using int64 = type<std::int64_t>;
+            using detail::epoch_offset;
+            using std::chrono::duration_cast;
+            using std::chrono::microseconds;
+
+            const auto value = co_await int64::from_sql(size, reader);
+            const auto micros = microseconds(value) + epoch_offset;
+            const auto duration = duration_cast<Duration>(micros);
+
+            co_return time_point(duration);
+        }
 
         static auto to_sql(
-            const timestamp time,
+            const time_point time,
             writer& writer
-        ) -> ext::task<>;
+        ) -> ext::task<> {
+            using int64 = type<std::int64_t>;
+            using detail::epoch_offset;
+            using std::chrono::duration_cast;
+            using std::chrono::microseconds;
 
-        static constexpr auto size(const timestamp time) -> std::int32_t {
+            const auto time_since_epoch = time.time_since_epoch();
+            const auto micros = duration_cast<microseconds>(time_since_epoch);
+            const auto converted = micros - epoch_offset;
+            const auto value = converted.count();
+
+            co_await int64::to_sql(value, writer);
+        }
+
+        static constexpr auto size(const time_point time) -> std::int32_t {
             return sizeof(std::int64_t);
         }
     };
--- src/libpg++/sql/type/timestamp.test.cpp
+++ src/libpg++/sql/type/timestamp.test.cpp
@@ -5,13 +5,15 @@
 #include <sstream>
 
 namespace {
+    using timestamp = std::chrono::system_clock::time_point;
+
     constexpr auto format_string = "%Y-%m-%d %H:%M:%S";
     constexpr auto time_zone = -5;
 }
 
 class TimestampTest : public pg::test::TypeTest {
-    auto format(pg::timestamp ts) -> std::string {
-        const auto time_t = pg::timestamp::clock::to_time_t(ts);
+    auto format(timestamp ts) -> std::string {
+        const auto time_t = timestamp::clock::to_time_t(ts);
         const auto* tm = localtime(&time_t);
 
         auto stream = std::ostringstream();
@@ -20,20 +22,20 @@ class TimestampTest : public pg::test::TypeTest {
         return stream.str();
     }
 
-    auto read(std::string_view timestamp) -> pg::timestamp {
-        auto result = pg::timestamp();
+    auto read(std::string_view string) -> timestamp {
+        auto result = timestamp();
 
         run([&]() -> ext::task<> {
-            result = co_await client.fetch<pg::timestamp>(fmt::format(
+            result = co_await client.fetch<timestamp>(fmt::format(
                 "SELECT '{}'::timestamptz",
-                timestamp
+                string
             ));
         }());
 
         return result;
     }
 
-    auto write(pg::timestamp ts) -> std::string {
+    auto write(timestamp ts) -> std::string {
         auto result = pg::result();
 
         run([&]() -> ext::task<> {
@@ -55,7 +57,7 @@ protected:
         EXPECT_EQ(timestamp, string);
     }
 
-    auto write_time(pg::timestamp ts, std::string_view expected) -> void {
+    auto write_time(timestamp ts, std::string_view expected) -> void {
         const auto result = write(ts);
 
         EXPECT_EQ(expected, result);
@@ -71,7 +73,7 @@ TEST_F(TimestampTest, Write) {
     using std::chrono::sys_days;
 
     const auto date = March / 8 / 2022;
-    const pg::timestamp ts = sys_days{date};
+    const timestamp ts = sys_days{date};
 
     write_time(ts, "2022-03-07 19:00:00-05");
 }
