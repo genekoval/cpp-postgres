b27f666e813ff2425ce4b7012dc66237be87e586
Gene Koval <genyaremchuk@protonmail.com>
2023-03-22 02:16:43 -0400

    Make transaction's rollback_task a free function

--- include/pg++/client/transaction.hpp
+++ include/pg++/client/transaction.hpp
@@ -6,8 +6,6 @@ namespace pg {
     class transaction {
         std::shared_ptr<netcore::mutex<detail::connection>> handle;
         bool done = false;
-
-        auto rollback_task() -> ext::detached_task;
     public:
         explicit transaction(
             std::shared_ptr<netcore::mutex<detail::connection>> connection
--- src/libpg++/client/transaction.cpp
+++ src/libpg++/client/transaction.cpp
@@ -2,6 +2,29 @@
 
 #include <cassert>
 
+namespace {
+    auto rollback(
+        std::shared_ptr<netcore::mutex<pg::detail::connection>> handle
+    ) -> ext::detached_task {
+        try {
+            auto connection = co_await handle->lock();
+            co_await connection->exec("ROLLBACK");
+
+            TIMBER_DEBUG("{} rollback transaction", *connection);
+        }
+        catch (const std::exception& ex) {
+            TIMBER_ERROR(
+                "{} rollback failed: {}",
+                handle->get(),
+                ex.what()
+            );
+        }
+        catch (...) {
+            TIMBER_ERROR("{} rollback failed", handle->get());
+        }
+    }
+}
+
 namespace pg {
     transaction::transaction(
         std::shared_ptr<netcore::mutex<detail::connection>> connection
@@ -14,7 +37,7 @@ namespace pg {
     {}
 
     transaction::~transaction() {
-        if (handle && !done) rollback_task();
+        if (handle && !done) ::rollback(std::move(handle));
     }
 
     auto transaction::operator=(transaction&& other) -> transaction& {
@@ -38,26 +61,5 @@ namespace pg {
         done = true;
 
         TIMBER_DEBUG("{} rollback transaction", *connection);
-    }
-
-    auto transaction::rollback_task() -> ext::detached_task {
-        auto handle = std::move(this->handle);
-
-        try {
-            auto connection = co_await handle->lock();
-            co_await connection->exec("ROLLBACK");
-
-            TIMBER_DEBUG("{} rollback transaction", *connection);
-        }
-        catch (const std::exception& ex) {
-            TIMBER_ERROR(
-                "{} rollback failed: {}",
-                handle->get(),
-                ex.what()
-            );
-        }
-        catch (...) {
-            TIMBER_ERROR("{} rollback failed", handle->get());
-        }
     }
 }
